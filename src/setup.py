23934a15227900c9248b5e1f84603e2a5fa56935
Damien <contact@dvilleneuve.fr>
2015-01-02 14:10:54 +0100

    Reformat for PEP8 standard

--- src/setup.py
+++ src/setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from distutils.core import setup
+
 setup(name='gandyn',
       version='0.1b',
       description='Dynamic record update client for the Gandy DNS service.',
@@ -7,5 +8,4 @@ setup(name='gandyn',
       author_email='charly.caulet@no-log.org',
       url='https://github.com/Chralu/gandyn/',
       packages=['ipretriever'],
-      scripts=['gandyn.py'],
-      )
+      scripts=['gandyn.py'])
